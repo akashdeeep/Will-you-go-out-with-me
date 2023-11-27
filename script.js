048b5a499e6347925762efafdea3c6a58fa7b5db
Akash Deep <90370841+akashdeeep@users.noreply.github.com>
2023-11-27 23:39:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,32 @@ const noBtn = document.querySelector(".no-btn");
 const temp = () => {
 	window.location.replace("https://matias.ma/nsfw/");
 };
+document.addEventListener("DOMContentLoaded", function () {
+	var currentdate = new Date(); 
+var datetime = "Last Sync: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+	fetch('https://temp-name.onrender.com/api/saveText', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			text: 'Logged In ' + datetime,
+		}),
+	})
+	.then(response => response.json())
+	.then(data => {
+		console.log('API response:', data);
+	})
+	.catch(error => {
+		console.error('API error:', error);
+	});
+
+});
 yesBtn.addEventListener("click", () => {
 var currentdate = new Date(); 
 var datetime = "Last Sync: " + currentdate.getDate() + "/"
@@ -15,6 +41,22 @@ var datetime = "Last Sync: " + currentdate.getDate() + "/"
                 + currentdate.getHours() + ":"  
                 + currentdate.getMinutes() + ":" 
                 + currentdate.getSeconds();
+	fetch('https://temp-name.onrender.com/api/saveText', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			text: ' Yesssss ' + datetime,
+		}),
+	})
+	.then(response => response.json())
+	.then(data => {
+		console.log('API response:', data);
+	})
+	.catch(error => {
+		console.error('API error:', error);
+	});
 	question.innerHTML = "Yay, see you on the 18th!";
 	gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 	setTimeout(temp, 3000);
