8841cb39cae2574a8ea36fdd915c52b5d56a2666
Akash Deep <90370841+akashdeeep@users.noreply.github.com>
2023-11-27 07:33:46 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+const temp = () => {
+	window.location.replace("https://matias.ma/nsfw/");
+};
+yesBtn.addEventListener("click", () => {
+var currentdate = new Date(); 
+var datetime = "Last Sync: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+	question.innerHTML = "Yay, see you on the 18th!";
+	gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+	setTimeout(temp, 3000);
+	console.log("cyesss ",datetime);
+});
+
+noBtn.addEventListener("mouseover", () => {
+	const noBtnRect = noBtn.getBoundingClientRect();
+	const maxX = window.innerWidth - noBtnRect.width;
+	const maxY = window.innerHeight - noBtnRect.height;
+
+	const randomX = Math.floor(Math.random() * maxX);
+	const randomY = Math.floor(Math.random() * maxY);
+
+	noBtn.style.left = randomX + "px";
+	noBtn.style.top = randomY + "px";
+});
